23616792d853a1c082011b4dba8f4c97701618dd
MichaelTretiak <mtretiak@me.com>
2025-03-03 15:28:46 -0700

    Fix deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
+  output: 'standalone',
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
 }
 
